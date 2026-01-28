0d5a07449f126c48fff39f6c5e16f3f6486e8c54
nkhondokar2420136-creator <nkhondokar2420136@bscse.uiu.ac.bd>
2026-01-28 22:56:36 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['recharts'],
+  ssr: {
+    noExternal: ['recharts']
   },
-  build: {
-    commonjsOptions: {
-      include: [/recharts/, /node_modules/],
-    },
+  optimizeDeps: {
+    include: ['recharts']
+  }
   server: {
     proxy: {
       '/api': {
